107e53da5317a941f45618ddd1285160672f0268
diegoalexh <diegoalexh@gmail.com>
2018-05-24 17:17:06 -0300

    Update environment.js

--- server/environment/environment.js
+++ server/environment/environment.js
@@ -1,10 +1,10 @@
 const env = { 
 	dbName: 'preventiondb', 
 	dbFileName: 'filesdb',
-	key : 'qz9uH5rIle2JJNb7g5Q4jCAoO3A5LeLPAxeRzdRDd31S5eJtDiky5IZ393W7MfVpdwlorlm35q7eQZd5GU6IXw==' , 
-	port: 10255,
-	OcpApimSubscriptionKey : '8d17e1ab776a4f9fbb2b53aa5370f9c1',
-	predictionkey : '04f6290c870a4d3595e6004c26d65762',
+	key : '' , 
+	port: ,
+	OcpApimSubscriptionKey : '',
+	predictionkey : '',
 }
 
 module.exports = env;
